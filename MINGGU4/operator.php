8feefd3baacd9d2796b1a26fdc0b44a191128883
esapratama <ptrprtm16@gmail.com>
2024-09-22 12:16:25 +0700

    first commit

---
+++ MINGGU4/operator.php
@@ -0,0 +1,18 @@
+<?php 
+    $a = 10;
+    $b = 5;
+
+    $hasilTambah = $a + $b;
+    $hasilKurang = $a - $b;
+    $hasilKali = $a * $b;
+    $hasilBagi = $a / $b;
+    $sisaBagi = $a % $b;
+    $pangkat = $a ** $b;
+
+    $hasilSama = $a == $b;
+    $hasilTidakSama = $a != $b;
+    $hasilLebihKecil = $a < $b;
+    $hasilLebihBesar = $a > $b;
+    $hasilLebihKecilSama = $a <= $b;
+    $hasilLebihBesarSama = $a >= $b;
+?>
---
+++ MINGGU4/strukturkontrol.php
@@ -0,0 +1,45 @@
+<?php 
+    $nilaiNumerik = 92;
+
+    if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+        echo "Nilai huruf: A";
+    } elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+        echo "Nilai huruf: B";
+    } elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+        echo "Nilai huruf: C";
+    } elseif ($nilaiNumerik < 70) {
+        echo "Nilai huruf: D";
+    }
+
+    $jarakSaatIni = 0;
+    $jarakTarget = 500;
+    $peningkatanHarian = 30;
+    $hari = 0;
+
+    while ($jarakSaatIni < $jarakTarget) {
+        $jarakSaatIni += $peningkatanHarian;
+        $hari++;
+    }
+    
+    echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+    $jumlahLahan = 10;
+    $tanamanPerlahan = 5;
+    $buahPerTanaman = 10;
+    $jumlahBuah = 0;
+
+    for ($i = 1; $i <= $jumlahLahan; $i++) {
+        $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
+    }
+
+    echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+    $skorUjian = [85, 92, 78, 96, 88];
+    $totalSkor = 0;
+
+    foreach ($skorUjian as $skor) {
+        $totalSkor += $skor;
+    }
+
+    echo "Total skor ujian adalah: "
+?>
